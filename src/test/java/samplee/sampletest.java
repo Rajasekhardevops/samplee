677049d2d571d984de41547653bb74fa46ccc529
Rajasekhardevops <gaddamrajasekhar512@gmail.com>
2019-06-28 10:17:42 +0530

    hii this new commit

---
+++ src/test/java/samplee/sampletest.java
@@ -0,0 +1,14 @@
+package samplee;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sampletest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
